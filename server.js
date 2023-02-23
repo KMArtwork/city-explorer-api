d003fe97eaa097fff2e617e4668865c20ea6feb7
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-22 17:38:23 -0800

    complete lab08 task01 | make proxy requests to weatherbit api

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express'); //loads express library from node_modules
 const cors = require('cors');
 const app = express(); // creates a singleton
 require('dotenv').config();
-
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
 
 const PORT = process.env.PORT;
+const WEATHERBIT_ACCESS_KEY = process.env.WEATHERBIT_ACCESS_KEY;
 
 class Forecast {
   constructor(date, description) {
@@ -19,29 +19,33 @@ class Forecast {
 
 app.use(cors());
 
-app.get('/weather', (request, response) => {
-  let queryMatch = weatherData.find(element => {
-    if (request.query.city.toLowerCase().includes(element.city_name.toLowerCase())  
-        || (request.query.lat === element.lat && request.query.lon === element.lon)) {
-      return element;
-    }
-  })
 
-    if (queryMatch){
-      let forecastArr = [];
-      queryMatch.data.forEach(element => {
-        forecastArr.push(new Forecast(
-          element.datetime,
-          `Expect ${element.weather.description} | Low of ${element.low_temp} | High of ${element.high_temp}`))
+app.get('/weather', async (request, response) => {
+  //request.query.city | request.query.lat | request.query.lon
+  
+  try {
+    let proxyRequest = {
+      url: `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&city=${request.query.city}&key=${WEATHERBIT_ACCESS_KEY}&days=7`,
+      method: 'GET'
+    }
+
+    let proxyResponse = [];
+    
+    axios(proxyRequest)
+      .then((axiosResponse) => {
+        axiosResponse.data.data.forEach(element => {
+          proxyResponse.push(new Forecast(
+            element.datetime, 
+            `Expect ${element.weather.description} | High of ${element.high_temp} | Low of ${element.low_temp}`));
+        });
+        response.status(200).send(proxyResponse);
       })
-      response.status(200).send(forecastArr);
-    } 
-    else if(!queryMatch) {
-      response.status(500).send('Server Side Error');
-    }
+
+  } catch (err) {
+    response.status(500).send('Could not complete proxy request to weatherbit');
+  }
+
 })
-
-
 
 app.listen(PORT, () => {
   console.log('Server is up');
