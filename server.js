8aaa3adc983cc5de19cab27d0d046f68d06104f6
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-22 18:47:50 -0800

    successful proxy request to themoviedb

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const axios = require('axios');
 
 const PORT = process.env.PORT;
 const WEATHERBIT_ACCESS_KEY = process.env.WEATHERBIT_ACCESS_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
   }
+}
 
+class Movie {
+  constructor(title, overview, avgVotes, totalVotes, imageUrl, rating, releaseDate) {
+    this.title = title;
+    this.overview = overview;
+    this.avgVotes = avgVotes;
+    this.totalVotes = totalVotes;
+    this.imageUrl = imageUrl;
+    this.rating = rating;
+    this.releaseDate = releaseDate;
+  }
 }
 
 app.use(cors());
@@ -45,6 +57,28 @@ app.get('/weather', async (request, response) => {
     response.status(500).send('Could not complete proxy request to weatherbit');
   }
 
+})
+
+app.get('/movies', async (request, response) => {
+  try {
+    let proxyRequest = {
+      url: `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${request.query.city}&include_adult=false`,
+      method: 'GET'
+    }
+
+    let proxyResponse = [];
+    
+    axios(proxyRequest)
+      .then((axiosResponse) => {
+        axiosResponse.data.results.forEach(movie => {
+          proxyResponse.push(new Movie(movie.title, movie.overview, movie.vote_average, movie.vote_count, movie.poster_path, movie.popularity, movie.release_date));
+        });
+        response.status(200).send(proxyResponse);
+      })
+
+  } catch (err) {
+    response.status(500).send('Could not complete proxy request to themoviedb');
+  }
 })
 
 app.listen(PORT, () => {
